1ff984c1a0a9bce73e91751ec9a6421a36debfa6
Hitishaa <89304585+Hitisha-G@users.noreply.github.com>
2022-10-07 17:30:55 +0530

    Update AirDrop.sol

--- AirDrop.sol
+++ AirDrop.sol
@@ -1,1 +1,40 @@
-
+ pragma solidity ^0.4.17;
+ 
+ //Contract creation file
+ 
+ contract Airdrop{
+     
+     address public owner;
+     address[] public members;
+     
+     constructor() public{
+         owner = msg.sender;
+     }
+     
+     function enter() public payable{
+         
+         require(msg.value > 0.1 ether);
+         members.push(msg.sender);
+     }
+     
+     function random() private view returns(uint){
+         return uint(keccak256(abi.encodePacked(block.difficulty, now, members)));
+     }
+     
+     function pickWinner() public OnlyOwner{
+         
+         uint index = random() % members.length;
+         members[index].transfer(this.balance);
+         members = new address[](0);
+     }
+     
+     modifier OnlyOwner(){
+         require(msg.sender == owner);
+         _;
+     }
+     
+     function getMember() public view returns(address[]){
+         return members;
+     } 
+     
+ }
